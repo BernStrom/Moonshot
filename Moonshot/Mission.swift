0fa066c788e511ea8a81d9bb3e41750b9a6dc176
Bern <bernnystrom@gmail.com>
2022-07-06 17:32:49 +0800

    🚧 (chore): Add a Mission struct for holding JSON data from missions
    Created a new Mission.swift file for storing the decoded JSON data in a Mission struct from missions.json file.

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Bern N on 7/6/22.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
