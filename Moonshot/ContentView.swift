70ecd8bde2de02b020f0b09ca0be8b855fdb4748
Bern <bernnystrom@gmail.com>
2022-07-12 18:30:24 +0800

    💄 (new): Add custom color extension and improve app UI layout and colors
    Added two custom colors - lightBackground & darkBackground to as extension to Color and modified the app UI with an improved layout and dark-themed colors.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -28,21 +28,34 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundColor(.white)
                                     
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
