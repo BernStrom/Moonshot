91ad936bfa18fda4eb05edf5316c4e751f7bf4f8
Bern <bernnystrom@gmail.com>
2022-07-12 18:00:57 +0800

    🚧 (chore): Add user-friendly date formatting for mission dates
    Added a more user-friendly date formatting for the mission dates that displays according to users region date format preference with an initial layout of the app interface.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,11 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let mission: [Mission] = Bundle.main.decode("missions.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
-        Text("\(astronauts.count)")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -14,7 +14,7 @@ struct Mission: Codable, Identifiable {
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     
@@ -24,5 +24,9 @@ struct Mission: Codable, Identifiable {
     
     var image: String {
         "Apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
 }
